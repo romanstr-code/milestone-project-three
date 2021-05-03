c6ece3183466c9cabcd77707954dc921170a65ea
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-05-03 15:32:53 +0000

    Added dropdown structure for navbar and mobile nav.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    // Side Nav Right
+     $('.sidenav').sidenav({edge: "right"});
+     // DropDown button
+     $('.dropdown-trigger').dropdown();
     // Full page slider // Home Page
     $('.slider').slider();
     // Tool Tip
