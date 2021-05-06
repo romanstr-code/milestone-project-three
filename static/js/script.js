e3b4672f2a57c92b1d2a7ac15fcb7099570ff463
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-05-06 15:07:09 +0000

    Add Script for French Press Page Collapsible Accordion as Well as Materila Box script for page image to enlarge

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // Side Nav Right
-     $('.sidenav').sidenav({edge: "right"});
-     // DropDown button
-     $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav({ edge: "right" });
+    // DropDown button
+    $('.dropdown-trigger').dropdown();
     // Tool Tip
     $('.tooltipped').tooltip();
     // Delete recipe modal // Recipe Page
-     $('.modal').modal();
-     // Paralax Home page
-     $('.parallax').parallax();
-     // Carousel Home page
-     $('.carousel').carousel();
-     // Slider Home Page
-     $('.slider').slider();
-     
-     // Scroll down on Recipes Page
-     $(".down").click(function() {
-     $('html, body').animate({
-         scrollTop: $(".up").offset().top
-     }, 2000);
- });
+    $('.modal').modal();
+    // Paralax Home page
+    $('.parallax').parallax();
+    // Carousel Home page
+    $('.carousel').carousel();
+    // Slider Home Page
+    $('.slider').slider();
 
+    // Scroll down on Recipes Page
+    $(".down").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".up").offset().top
+        }, 2000);
+    });
  
-  });
+    // Collapsible Accordion // 
+     $('.collapsible').collapsible();
 
-  
+     // Material Box Recipe Page
+     $('.materialboxed').materialbox();
+});
